6505c4a448fc8ba38dc0257972ebbc061202a8ca
mtjsoft <1279164594@qq.com>
2017-05-06 10:17:11 +0800

    Signed-off-by: mtjsoft <1279164594@qq.com>

---
+++ src/com/seeyon/line/MainActivity.java
@@ -0,0 +1,45 @@
+package com.seeyon.line;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.seeyon.line.view.ContentView;
+import com.seeyon.line.view.Drawl.GestureCallBack;
+
+/**
+ * 
+ * @author yinqiaoyin
+ * 
+ */
+public class MainActivity extends Activity {
+
+	private FrameLayout body_layout;
+	private ContentView content;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		body_layout = (FrameLayout) findViewById(R.id.body_layout);
+
+
+		// 初始化一个显示各个点的viewGroup
+		content = new ContentView(this, "123654789",new GestureCallBack() {
+
+			@Override
+			public void checkedSuccess() {
+				Toast.makeText(MainActivity.this,"校验成功", Toast.LENGTH_SHORT).show();
+			}
+
+			@Override
+			public void checkedFail() {
+				Toast.makeText(MainActivity.this,"校验失败", Toast.LENGTH_SHORT).show();
+			}
+		});
+		//设置手势解锁显示到哪个布局里面
+		content.setParentView(body_layout);
+	}
+
+}
---
+++ src/com/seeyon/line/ScreenUtils.java
@@ -0,0 +1,16 @@
+package com.seeyon.line;
+
+import android.content.Context;
+import android.view.WindowManager;
+
+public class ScreenUtils {
+
+	@SuppressWarnings("deprecation")
+	public static int[] getScreenDispaly(Context context){
+		WindowManager wm=(WindowManager)context.getSystemService(Context.WINDOW_SERVICE);
+		int width=wm.getDefaultDisplay().getWidth();//手机屏幕的宽度
+		int height=wm.getDefaultDisplay().getHeight();//手机屏幕的高度
+		int result[] = {width,height};
+		return result;
+	}
+}
